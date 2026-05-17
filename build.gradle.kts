cbe029492290ad21da47f42dfa29fc72f2e80ee4
1-alisa <walisasofia@gmail.com>
2026-05-17 14:23:29 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+
+buildscript {
+    extra.apply {
+        set("lifecycle_version", "2.6.2")
+        set("room_version", "2.6.1")
+    }
+}
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("com.android.library") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+}
